abb0d13da43f2769481d59e446e91e79e5c89438
Israel Matos <israel.bn@gmail.com>
2018-09-23 16:37:32 -0300

    Primeira classe de dominio adicionada Classificacao

---
+++ src/main/java/com/israel/orcamento/domain/Classificacao.java
@@ -0,0 +1,59 @@
+package com.israel.orcamento.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Classificacao implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private  String nome;
+    private  String classificacao;
+
+    public Classificacao() {
+    }
+
+    public Classificacao(Integer id, String nome, String classificacao) {
+        this.id = id;
+        this.nome = nome;
+        this.classificacao = classificacao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(String classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Classificacao that = (Classificacao) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/israel/orcamento/resources/ClassificacaoResource.java
+++ src/main/java/com/israel/orcamento/resources/ClassificacaoResource.java
@@ -1,15 +1,27 @@
 package com.israel.orcamento.resources;
 
+import com.israel.orcamento.domain.Classificacao;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/classificacoes")
 public class ClassificacaoResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST está funcionando!";
+    public List<Classificacao> listar() {
+        Classificacao cla1 = new Classificacao(1, "Alimentação", "Almoço");
+        Classificacao cla2 = new Classificacao(1, "Outras", "Outras");
+
+        List<Classificacao> lista = new ArrayList<>();
+
+        lista.add(cla1);
+        lista.add(cla2);
+
+        return lista;
     }
 }
