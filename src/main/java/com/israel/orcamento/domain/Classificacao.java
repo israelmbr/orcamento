9c8c4800e7f5e0e96458fca6a16d44071319f02a
Israel Matos <israel.bn@gmail.com>
2018-09-23 17:03:42 -0300

    Banco de dados H2 e criação do banco automaticamente

--- src/main/java/com/israel/orcamento/domain/Classificacao.java
+++ src/main/java/com/israel/orcamento/domain/Classificacao.java
@@ -1,11 +1,20 @@
 package com.israel.orcamento.domain;
 
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Classificacao implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private  String nome;
     private  String classificacao;
